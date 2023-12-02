44ac5dc6f0d990ece8cf443c0229d40723b9b675
Starinin Andrey <starininandrey@gmail.com>
2023-12-02 14:15:30 +0300

    Update

--- AccountsManager.App/Program.cs
+++ AccountsManager.App/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AccountsManager.Config;
+using AccountsManager.DbLib;
+
+var config = AppSettings.Init();
+
+using var db = new DataContext(config.ConnectionString, config.Logger, config.LogLevel);
+
+do
+{
+    foreach (var a in db.Accounts.ToList())
+    {
+        var role = a.IdRoleNavigation.RoleName;
+        var user = a.User;
+        Console.Write($"{a.Login} ({a.Password}) [{role}] -> {user.LastName} {user.FirstName} ({user.Email}): ");
+        foreach (var p in user.ProjectsUsers)
+        {
+            Console.Write($"{p.IdProjectNavigation.Title}\t");
+        }
+        Console.WriteLine();
+    }
+
+    Console.ReadKey();
+} while (true);
